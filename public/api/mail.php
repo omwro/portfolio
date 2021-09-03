cd50b70a3098517f5db91f1aec1f3eac232797d6
Omer Erdem <omer.erdem@hva.nl>
2021-09-03 21:00:55 +0200

    Remove old PHP api files

--- public/api/mail.php
+++
@@ -1,9 +0,0 @@
-<?php
-$txt = "Name: " . $_POST['name'] . "\n" .
-    "Email: " . $_POST['email'] . "\n" .
-    "Subject: " . $_POST['subject'] . "\n\n" .
-    $_POST['message'];
-$to = "info@omererdem.nl";
-$subject = "Contactform Omererdem.nl";
-$headers = "From: webmaster@omererdem.nl" . "\r\n" . "CC: m.o.erdem@outlook.com";
-mail($to, $subject, $txt, $headers);
