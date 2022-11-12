b6d93f4bf69822b7eb3640f930c0b87517c2ab75
Omer Erdem <m.o.erdem@outlook.com>
2022-11-12 09:11:20 +0100

    removing tailwind that was breaking cicd

--- src/main.js
+++ src/main.js
@@ -1,52 +1,87 @@
-import Vue from 'vue'
-import App from './App.vue'
-import axios from 'axios'
-import VueAxios from 'vue-axios'
-import {library} from '@fortawesome/fontawesome-svg-core'
-import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
-import {faAt, faStar, faStarHalfAlt, faHandPointer, faExternalLinkAlt, faGlobe} from '@fortawesome/free-solid-svg-icons'
-import {faStar as faStarEmpty} from '@fortawesome/free-regular-svg-icons'
-import {faFontAwesome, faLinkedin, faSkype, faDiscord, faCodepen, faGithub} from '@fortawesome/free-brands-svg-icons'
-import VueLazyload from 'vue-lazyload'
-import i18n from './i18n'
-import VueRouter from 'vue-router';
-import Index from './pages/Index'
-import CV from './pages/CV'
-import Vuex from 'vuex'
-import GitFlow from 'gitflow-component'
-import createPersistedState from "vuex-persistedstate"
-import './assets/tailwind.css'
+import Vue from "vue";
+import App from "./App.vue";
+import axios from "axios";
+import VueAxios from "vue-axios";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import {
+    faAt,
+    faStar,
+    faStarHalfAlt,
+    faHandPointer,
+    faExternalLinkAlt,
+    faGlobe,
+} from "@fortawesome/free-solid-svg-icons";
+import { faStar as faStarEmpty } from "@fortawesome/free-regular-svg-icons";
+import {
+    faFontAwesome,
+    faLinkedin,
+    faSkype,
+    faDiscord,
+    faCodepen,
+    faGithub,
+} from "@fortawesome/free-brands-svg-icons";
+import VueLazyload from "vue-lazyload";
+import i18n from "./i18n";
+import VueRouter from "vue-router";
+import Index from "./pages/Index";
+import CV from "./pages/CV";
+import Vuex from "vuex";
+import GitFlow from "gitflow-component";
+import createPersistedState from "vuex-persistedstate";
 
-library.add(faAt, faLinkedin, faSkype, faDiscord, faCodepen, faGithub, faFontAwesome, faStar, faStarHalfAlt, faStarEmpty, faHandPointer, faExternalLinkAlt, faGlobe)
+library.add(
+    faAt,
+    faLinkedin,
+    faSkype,
+    faDiscord,
+    faCodepen,
+    faGithub,
+    faFontAwesome,
+    faStar,
+    faStarHalfAlt,
+    faStarEmpty,
+    faHandPointer,
+    faExternalLinkAlt,
+    faGlobe
+);
 
-Vue.config.productionTip = false
-Vue.use(VueAxios, axios)
-Vue.use(VueLazyload)
-Vue.component('font-awesome-icon', FontAwesomeIcon)
-Vue.use(VueRouter)
-Vue.use(Vuex)
-Vue.use(GitFlow)
+Vue.config.productionTip = false;
+Vue.use(VueAxios, axios);
+Vue.use(VueLazyload);
+Vue.component("font-awesome-icon", FontAwesomeIcon);
+Vue.use(VueRouter);
+Vue.use(Vuex);
+Vue.use(GitFlow);
 
-const routes = [{path: '/', component: Index}, {path: '/cv', component: CV}, {path: '*', component: Index}]
+const routes = [
+    { path: "/", component: Index },
+    { path: "/cv", component: CV },
+    { path: "*", component: Index },
+];
 
-const router = new VueRouter({routes, mode: 'history'})
+const router = new VueRouter({ routes, mode: "history" });
 
 const store = new Vuex.Store({
     state: {
         // darkmode: window.matchMedia && window.matchMedia('prefers-color-scheme: dark').matches,
         darkmode: false,
         menu: false,
-    }, mutations: {
-        toggleDarkmode(state) {
-            state.darkmode = !state.darkmode
-        }, toggleMenu(state) {
-            state.menu = !state.menu
-        }
     },
-    plugins: [createPersistedState()]
-})
-
+    mutations: {
+        toggleDarkmode(state) {
+            state.darkmode = !state.darkmode;
+        },
+        toggleMenu(state) {
+            state.menu = !state.menu;
+        },
+    },
+    plugins: [createPersistedState()],
+});
 
 new Vue({
-    router, store, render: h => h(App), i18n
-}).$mount('#app')
+    router,
+    store,
+    render: (h) => h(App),
+    i18n,
+}).$mount("#app");
--- src/views/Header.vue
+++ src/views/Header.vue
@@ -1,95 +1,93 @@
 <template>
-	<div
-		id="header"
-		class="fixed top-0 left-0 right-0 z-10 flex items-center justify-between h-8 p-3">
-		<div class="header-left">
-			<a href="#home">
-				<img
-					alt="logo"
-					class="logo"
-					src="../../public/img/company/logo.webp" />
-			</a>
-		</div>
-		<div class="header-right flex-nowrap flex">
-			<nav class="navbar">
-				<template v-for="n in nav">
-					<a v-if="n.name !== 'Home'" :key="n.name" :href="n.href">
-						{{ $t(n.translate) }}
-					</a>
-				</template>
-				<template v-if="dark">
-					<a>
-						<img
-							@click="onDarkModeToggle"
-							src="img/actions/sun.png"
-							class="icon"
-							alt="sun" />
-					</a>
-				</template>
-				<template v-else>
-					<a>
-						<img
-							@click="onDarkModeToggle"
-							src="img/actions/moon.png"
-							class="icon"
-							alt="moon" />
-					</a>
-				</template>
-				<template v-if="lan === 'nl'">
-					<a>
-						<img
-							@click="setLanguage('en')"
-							src="img/actions/usa.png"
-							class="icon"
-							alt="en" />
-					</a>
-				</template>
-				<template v-else>
-					<a>
-						<img
-							@click="setLanguage('nl')"
-							src="img/actions/nl.png"
-							class="icon"
-							alt="en" />
-					</a>
-				</template>
-			</nav>
-			<div class="hamburger" @click="openMenu">
-				<span class="hamburger-top"></span>
-				<span class="hamburger-middle"></span>
-				<span class="hamburger-bottom"></span>
-			</div>
-		</div>
-	</div>
+    <div id="header">
+        <div class="header-left">
+            <a href="#home">
+                <img
+                    alt="logo"
+                    class="logo"
+                    src="../../public/img/company/logo.webp" />
+            </a>
+        </div>
+        <div class="header-right">
+            <nav class="navbar">
+                <template v-for="n in nav">
+                    <a v-if="n.name !== 'Home'" :key="n.name" :href="n.href">
+                        {{ $t(n.translate) }}
+                    </a>
+                </template>
+                <template v-if="dark">
+                    <a>
+                        <img
+                            @click="onDarkModeToggle"
+                            src="img/actions/sun.png"
+                            class="icon"
+                            alt="sun" />
+                    </a>
+                </template>
+                <template v-else>
+                    <a>
+                        <img
+                            @click="onDarkModeToggle"
+                            src="img/actions/moon.png"
+                            class="icon"
+                            alt="moon" />
+                    </a>
+                </template>
+                <template v-if="lan === 'nl'">
+                    <a>
+                        <img
+                            @click="setLanguage('en')"
+                            src="img/actions/usa.png"
+                            class="icon"
+                            alt="en" />
+                    </a>
+                </template>
+                <template v-else>
+                    <a>
+                        <img
+                            @click="setLanguage('nl')"
+                            src="img/actions/nl.png"
+                            class="icon"
+                            alt="en" />
+                    </a>
+                </template>
+            </nav>
+            <div class="hamburger" @click="openMenu">
+                <span class="hamburger-top"></span>
+                <span class="hamburger-middle"></span>
+                <span class="hamburger-bottom"></span>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
 import json from "../../public/data/navigation.json";
 
 export default {
-	name: "Header",
-	data: () => ({
-		nav: json,
-	}),
-	computed: {
-		dark() {
-			return this.$store.state.darkmode;
-		},
-		lan() {
-			return this.$i18n.locale;
-		},
-	},
-	methods: {
-		openMenu() {
-			this.$store.commit("toggleMenu");
-		},
-		onDarkModeToggle() {
-			this.$store.commit("toggleDarkmode");
-		},
-		setLanguage(lan) {
-			this.$i18n.locale = lan;
-		},
-	},
+    name: "Header",
+    data: () => ({
+        nav: json,
+    }),
+    computed: {
+        dark() {
+            return this.$store.state.darkmode;
+        },
+        lan() {
+            return this.$i18n.locale;
+        },
+    },
+    methods: {
+        openMenu() {
+            this.$store.commit("toggleMenu");
+        },
+        onDarkModeToggle() {
+            this.$store.commit("toggleDarkmode");
+        },
+        setLanguage(lan) {
+            this.$i18n.locale = lan;
+        },
+    },
 };
 </script>
 
@@ -97,95 +95,108 @@ export default {
 @import "../styles/variables";
 
 #app #header {
-	background-color: $background-light;
-	transition: 0.5s;
-	border-bottom: $accent 2px solid;
+    background-color: $background-light;
+    transition: 0.5s;
+    border-bottom: $accent 2px solid;
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    z-index: 10;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    height: 32px;
+    padding: 12px;
 
-	.header-left {
-		.logo {
-			max-height: 24px;
-			margin: auto;
-			cursor: pointer;
-		}
-	}
+    .header-left {
+        .logo {
+            max-height: 24px;
+            margin: auto;
+            cursor: pointer;
+        }
+    }
 
-	.header-right {
-		.navbar {
-			display: none;
+    .header-right {
+        display: flex;
+        flex-wrap: nowrap;
 
-			a {
-				color: black;
-				text-decoration: none;
-				font-size: 16px;
-				margin: 0 8px;
-				align-self: center;
+        .navbar {
+            display: none;
 
-				&:hover {
-					color: $accent;
-					border-bottom: solid 1px $accent;
-				}
-			}
+            a {
+                color: black;
+                text-decoration: none;
+                font-size: 16px;
+                margin: 0 8px;
+                align-self: center;
 
-			@media (min-width: $mq-m) {
-				display: flex;
-			}
-		}
+                &:hover {
+                    color: $accent;
+                    border-bottom: solid 1px $accent;
+                }
+            }
 
-		.hamburger {
-			width: 25px;
-			height: 25px;
-			padding-top: 5px;
-			display: flex;
-			flex-direction: column;
-			cursor: pointer;
+            @media (min-width: $mq-m) {
+                display: flex;
+            }
+        }
 
-			@media (min-width: $mq-m) {
-				display: none;
-			}
+        .hamburger {
+            width: 25px;
+            height: 25px;
+            padding-top: 5px;
+            display: flex;
+            flex-direction: column;
+            cursor: pointer;
 
-			span {
-				width: 100%;
-				height: 25%;
-				border-top: solid 3px;
-				transition: 0.3s;
-			}
+            @media (min-width: $mq-m) {
+                display: none;
+            }
 
-			&:hover .hamburger-top,
-			&:hover .hamburger-bottom {
-				width: 50%;
-				margin: 0 25%;
-				transition: 0.3s;
-			}
-		}
-	}
+            span {
+                width: 100%;
+                height: 25%;
+                border-top: solid 3px;
+                transition: 0.3s;
+            }
+
+            &:hover .hamburger-top,
+            &:hover .hamburger-bottom {
+                width: 50%;
+                margin: 0 25%;
+                transition: 0.3s;
+            }
+        }
+    }
 }
 
 .icon {
-	width: 24px;
-	height: 24px;
-	cursor: pointer;
+    width: 24px;
+    height: 24px;
+    cursor: pointer;
 }
 
 #app.dark #header {
-	background-color: $background-dark;
+    background-color: $background-dark;
 
-	&.ontop {
-		background-color: transparent;
-		transition: 0.5s;
-		border-bottom: none;
-	}
+    &.ontop {
+        background-color: transparent;
+        transition: 0.5s;
+        border-bottom: none;
+    }
 
-	.navbar a {
-		color: $color-dark;
+    .navbar a {
+        color: $color-dark;
 
-		&:hover {
-			color: $accent-light;
-			border-bottom-color: $accent-light;
-		}
-	}
+        &:hover {
+            color: $accent-light;
+            border-bottom-color: $accent-light;
+        }
+    }
 
-	.logo {
-		filter: invert(100%);
-	}
+    .logo {
+        filter: invert(100%);
+    }
 }
 </style>
