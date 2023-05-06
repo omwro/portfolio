910063e506baa8d66f0e7729838b88db5c506019
Omer Erdem <m.o.erdem@outlook.com>
2023-05-06 13:11:14 +0200

    adding socials view

---
+++ components/elements/ContactButton.vue
@@ -0,0 +1,29 @@
+<template>
+    <a
+        class="h-8 w-32 flex flex-row cursor-pointer no-underline m-2 rounded-[10px] border-2 group"
+        :href="url"
+        :style="[
+            { 'background-color': getColor() },
+            { 'border-color': getColor() },
+        ]">
+        <div class="w-8 flex justify-center items-center">
+            <Icon class="text-primary-text-light h-[18px] transition-[0.2s] group-hover:h-[22px] group-hover:transition-[0.2s]"
+                  :name="icon"
+                  size="32px"/>
+        </div>
+        <div class="flex self-center text-lg font-[bold] text-primary-text-light transition-[0.2s] pl-2 group-hover:text-[22px] group-hover:transition-[0.2s]">{{ text }}</div>
+    </a>
+</template>
+
+
+<script>
+export default {
+    name: "ContactButton",
+    props: ["icon", "text", "url", "color"],
+    methods: {
+        getColor() {
+            return `rgb(${this.color.r}, ${this.color.g}, ${this.color.b})`;
+        },
+    },
+};
+</script>
--- components/views/Socials.vue
+++ components/views/Socials.vue
@@ -1,13 +1,26 @@
 <template>
-    <h1>Socials</h1>
+    <section id="socials">
+        <h1>{{ $t("socials.title") }}</h1>
+        <p class="text-center">{{ $t("socials.desc") }}</p>
+        <div class="flex flex-row flex-wrap justify-center">
+            <ContactButton
+                v-for="(s, i) in socials"
+                :key="i"
+                :icon="s.icon"
+                :text="s.name"
+                :url="s.url"
+                :color="s.color" />
+        </div>
+    </section>
 </template>
+
+<script setup>
+import socials from "../../assets/json/contact.json";
+import ContactButton from "../elements/ContactButton";
+</script>
 
 <script>
 export default {
     name: "Socials"
 };
 </script>
-
-<style scoped>
-
-</style>
