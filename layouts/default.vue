9b1b49adfa94ea466b2dd1648fcf128e83ec153d
Omer Erdem <m.o.erdem@outlook.com>
2023-05-06 10:02:38 +0200

    Adding layout

---
+++ layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+    <Header/>
+    <slot/>
+    <Footer/>
+    <Menu/>
+</template>
+
+<script>
+import Header from "../components/views/Header";
+import Footer from "../components/views/Footer";
+import Menu from "../components/views/Menu";
+export default {
+    name: "default",
+    components: { Menu, Footer, Header }
+};
+</script>
+
+<style scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,17 @@
 <template>
-    <Header/>
-    <Home/>
-    <Companies/>
-    <About/>
-    <Skills/>
-    <Projects/>
-    <Timeline/>
-    <Socials/>
-    <Competitors/>
-    <Footer/>
-    <Menu/>
+    <NuxtLayout>
+        <Home/>
+        <Companies/>
+        <About/>
+        <Skills/>
+        <Projects/>
+        <Timeline/>
+        <Socials/>
+        <Competitors/>
+    </NuxtLayout>
 </template>
 
 <script setup>
-import Header from "../components/views/Header";
 import Timeline from "../components/views/Timeline";
 import Home from "../components/views/Home";
 import Companies from "../components/views/Companies";
@@ -22,8 +20,6 @@ import Skills from "../components/views/Skills";
 import Projects from "../components/views/Projects";
 import Socials from "../components/views/Socials";
 import Competitors from "../components/views/Competitors";
-import Footer from "../components/views/Footer";
-import Menu from "../components/views/Menu";
 </script>
 
 <script>
