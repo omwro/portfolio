ab12a278012d4ccc295c749e1cc13d7eda90ceb0
Omer Erdem <m.o.erdem@outlook.com>
2023-05-06 14:41:23 +0200

    adding footer

--- components/views/Footer.vue
+++ components/views/Footer.vue
@@ -1,13 +1,17 @@
 <template>
-    <h1>Footer</h1>
+    <footer id="footer" class="text-primary-text-dark bg-primary-block-light italic text-[0.9rem] text-center overflow-hidden px-0 py-2">
+        <div class="whitespace-nowrap px-2 py-0">
+            &copy; 1999 - {{ currentYear() }} Omer Erdem All Rights Reserved
+        </div>
+    </footer>
 </template>
+
+<script setup>
+const currentYear = () => new Date().getFullYear();
+</script>
 
 <script>
 export default {
     name: "Footer"
 };
 </script>
-
-<style scoped>
-
-</style>
