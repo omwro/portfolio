ee311acb342af0035e0d9e9e5cfe361930a9bd71
Omer Erdem <m.o.erdem@outlook.com>
2023-05-06 12:36:55 +0200

    Adding companies view

--- components/views/Companies.vue
+++ components/views/Companies.vue
@@ -1,6 +1,22 @@
 <template>
-    <h1>Companies</h1>
+    <div id="overflow-hidden bg-primary-bg-light px-0 py-16">
+        <div class="flex flex-row flex-wrap -rotate-3 bg-primary-block-light overflow-hidden justify-center w-[calc(106%)] ml-[-3%]">
+            <a
+                v-for="c in companies"
+                :key="c.name"
+                :href="c.url"
+                target="_blank"
+                rel="author"
+                class="self-center p-4">
+                <img :src="c.img" :alt="c.name" class="w-auto h-auto max-w-[128px] max-h-16 invert-[1]"/>
+            </a>
+        </div>
+    </div>
 </template>
+
+<script setup>
+import companies from "../../assets/json/companies.json";
+</script>
 
 <script>
 export default {
