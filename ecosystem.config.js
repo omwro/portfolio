01884c095907bd4a3a115b94c5f3b30699167cd2
Omer <m.o.erdem@outlook.com>
2023-05-21 16:22:03 +0200

    pm2 support

---
+++ ecosystem.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    apps: [
+        {
+            name: 'Portfolio',
+            port: '3000',
+            exec_mode: 'cluster',
+            instances: 'max',
+            script: './.output/server/index.mjs'
+        }
+    ]
+}
