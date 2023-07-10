9f4db18f6afb559930608333d983296c8abe09c6
Omer Erdem <m.o.erdem@outlook.com>
2023-07-10 21:34:00 +0200

    Correcting CV template and text

--- pages/cv.vue
+++ pages/cv.vue
@@ -27,8 +27,8 @@
                     <span class="color-grey">info@omererdem.nl</span>
                 </div>
                 <div class="px-0 py-1">
-                    <span class="font-bold">Tel: </span>
-                    <span class="color-grey">{{ $t("general.tel") }}</span>
+                    <span class="font-bold">Portfolio: </span>
+                    <span class="color-grey">www.omererdem.nl</span>
                 </div>
             </div>
             <div class="max-w-[330px]">
@@ -56,6 +56,9 @@
                         </div>
                         <div class="text-xs font-bold">{{ edu.company }}</div>
                         <div class="leading-[1.4]">{{ $t(edu.desc, 1, {locale: "en"}) }}</div>
+                        <div class="flex flex-row flex-wrap gap-1">
+                            <div v-for="stack in edu.stack" class="border rounded-lg px-2 py-0.5 w-fit">{{ getSkillDisplayName(stack) }}</div>
+                        </div>
                     </div>
                 </template>
             </div>
@@ -74,27 +77,33 @@
                     <div class="text-base font-bold">{{ $t(xp.role, 1, {locale: "en"}) }}</div>
                     <div class="text-xs font-bold">{{ xp.company }}</div>
                     <div class="leading-[1.4]">{{ $t(xp.desc, 1, {locale: "en"}) }}</div>
+                    <div class="flex flex-row flex-wrap gap-1">
+                        <div v-for="stack in xp.stack" class="border rounded-lg px-2 py-0.5 w-fit">{{ getSkillDisplayName(stack) }}</div>
+                    </div>
                 </div>
             </div>
         </div>
         <div class="flex flex-row flex-wrap md:flex-nowrap print:flex-nowrap">
-            <div class="half container skills-container">
-                <div class="container-title uppercase">Skills</div>
-                <div class="content">
-                    <div class="flex flex-row text-base items-center gap-1"
-                        v-for="cv in getCVSkills()"
-                        :key="cv.name">
-                        <IconWrapper :name="cv.name" size="64" />
-                        <div class="flex flex-col">
-                            <div>{{ cv.display_name }}</div>
-                            <div>{{ getExperienceString(cv, $t, false, "en") }}</div>
-                        </div>
-
-                    </div>
+            <div class="w-full flex flex-col gap-2">
+                <div class="flex gap-1 items-center bg-gray-200 rounded-[20px_5px] p-2 uppercase font-bold">
+                    <span class="inline-block w-[200px]">Languages</span>
+                    <IconWrapper v-for="icon in languageIcons" :name="icon" size="24"/>
+                </div>
+                <div class="flex gap-1 items-center bg-gray-200 rounded-[20px_5px] p-2 uppercase font-bold">
+                    <span class="inline-block w-[200px]">Frontend Frameworks</span>
+                    <IconWrapper v-for="icon in frontendFrameworkIcons" :name="icon" size="24"/>
+                </div>
+                <div class="flex gap-1 items-center bg-gray-200 rounded-[20px_5px] p-2 uppercase font-bold">
+                    <span class="inline-block w-[200px]">Backend Frameworks</span>
+                    <IconWrapper v-for="icon in backendFrameworkIcons" :name="icon" size="24"/>
+                </div>
+                <div class="flex gap-1 items-center bg-gray-200 rounded-[20px_5px] p-2 uppercase font-bold">
+                    <span class="inline-block w-[200px]">Databases & Tools</span>
+                    <IconWrapper v-for="icon in databasesIcons" :name="icon" size="24"/>
                 </div>
             </div>
             <div class="text-center text-sm my-0 basis-1/3">
-                <nuxt-img src="/img/company/qr.png" alt="qrcode" width="190" height="190" class="mx-auto"/>
+                <nuxt-img src="/img/company/qr.png" alt="qrcode" width="160" height="160" class="mx-auto"/>
                 <div>https://www.omererdem.nl</div>
             </div>
         </div>
@@ -104,17 +113,20 @@
 <script setup>
 import timeline from "../assets/json/timeline.json";
 import skills from "../assets/json/skills.json";
-import { getExperienceString } from "../composables/useSkills";
 import IconWrapper from "../components/elements/IconWrapper";
 import moment from "moment/moment";
 import {DATE_FORMAT, RAW_DATE_FORMAT} from "../composables/useGitFlow";
-const getEducation = () => [
-    ...timeline.filter((x) => x.type === "Study"),
-    ...timeline.filter((x) => x.type === "Minor")
-]
+
+const languageIcons = ["js", "php", "java", "python", "csharp", "ts", "html", "css"]
+const frontendFrameworkIcons = ["vue", "nuxt", "angular", "jquery", "alpine", "knockout", "sass", "tailwind", "bootstrap"]
+const backendFrameworkIcons = ["magento", "node", "spring", "laravel", "kotlin", "quarkus", "fastapi", "aspnet"]
+const databasesIcons = ["mysql", "mongodb", "firebase", "eventstore", "git", "docker"]
+
+const getEducation = () => timeline.filter((x) => x.type === "Study")
 const getExperience = () => timeline.filter((x) => x.type === "Internship" || x.type === "Work").slice(0, 4)
 const getCVSkills = () => skills.filter((x) => x.show_cv === true)
 const formatDate = (date) => moment(date, RAW_DATE_FORMAT).format(DATE_FORMAT);
+const getSkillDisplayName = (skill) => skills.find(x => x.name === skill) ? skills.find(x => x.name === skill).display_name : skill;
 </script>
 
 <style scoped>
@@ -133,7 +145,7 @@ html, body, #app, #cv {
     @apply text-xl font-bold text-center pb-1;
 }
 #cv .container .content {
-    @apply grid justify-between p-2.5 rounded-[40px_15px] gap-4 bg-white;
+    @apply grid justify-between p-2.5 rounded-[30px_15px_40px] gap-4 bg-white;
 }
 .container .content {
     @apply sm:grid-cols-2;
