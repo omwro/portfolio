7562a6d07c4819ac621a6148a5426458ad6afd30
Omer Erdem <m.o.erdem@outlook.com>
2023-07-18 12:38:51 +0200

    Correcting CV layout

---
+++ components/elements/Chip.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="border rounded-lg px-2 py-0.5 w-fit">{{ text }}</div>
+</template>
+
+<script setup>
+defineProps(["text"])
+</script>
--- pages/cv.vue
+++ pages/cv.vue
@@ -1,41 +1,41 @@
 <template>
     <div id="cv">
-        <div class="header flex flex-col md:flex-row print:flex-row mb-2 items-center">
-            <div class="flex items-center justify-center overflow-hidden w-[200px] h-[200px] mr-4 rounded-[60px_15px] bg-gray-200 place-content-center">
-                <nuxt-img width="190" height="190" quality="100" class="bg-white rounded-[55px_10px]"
-                    alt="profile picture"
-                    src="/img/selfie/selfie-squared.webp" />
+        <div class="header flex flex-col md:flex-row print:flex-row items-center gap-4 mb-4">
+            <div
+                class="flex items-center justify-center overflow-hidden w-[160px] h-[160px] rounded-[60px_15px] bg-gray-200 place-content-center">
+                <nuxt-img width="150" height="150" quality="100" class="bg-white rounded-[55px_10px]"
+                          alt="profile picture"
+                          src="/img/selfie/selfie-squared.webp"/>
             </div>
-            <div class="mr-4 pr-4 md:border-r border-r-gray-200">
-                <div class="text-xl font-black color-red uppercase">
-                    {{ $t("general.firstname") }}
+            <div class="flex flex-col gap-1 flex-grow">
+                <div class="text-xl font-black uppercase">
+                    {{ $t("general.firstname") }} {{ $t("general.lastname") }}
                 </div>
-                <div class="text-xl font-black color-blue uppercase">
-                    {{ $t("general.lastname") }}
-                </div>
-                <div class="px-0 py-1 text-base font-bold uppercase">{{ $t("general.study") }}</div>
-                <div class="px-0 py-1">
+                <div class="text-base font-bold uppercase">{{ $t("general.study") }}</div>
+                <div>
                     <span class="font-bold">Age: </span>
                     <span class="color-grey">{{ $t("general.age") }}</span>
                 </div>
-                <div class="px-0 py-1">
+                <div>
                     <span class="font-bold">Area: </span>
                     <span class="color-grey">{{ $t("general.area", 1, {locale: "en"}) }}</span>
                 </div>
-                <div class="px-0 py-1">
+                <div>
                     <span class="font-bold">Email: </span>
                     <span class="color-grey">info@omererdem.nl</span>
                 </div>
-                <div class="px-0 py-1">
+                <div>
                     <span class="font-bold">Portfolio: </span>
-                    <span class="color-grey">www.omererdem.nl</span>
+                    <span class="color-grey">omererdem.nl</span>
+                </div>
+                <div>
+                    <span class="font-bold">LinkedIn: </span>
+                    <span class="color-grey">linkedin.com/in/omer-erdem/</span>
                 </div>
             </div>
-            <div class="max-w-[330px]">
-                <div class="text-xl text-center mb-2 font-bold">ABOUT ME</div>
-                <div class="text-sm leading-[1.35] color-grey">
-                    {{ $t("about.paragraph2", 1, {locale: "en"}) }}
-                </div>
+            <div>
+                <nuxt-img src="/img/company/qr.png" alt="qrcode" width="140" height="140" class="mx-auto"/>
+                <div>Explore more at my portfolio website</div>
             </div>
         </div>
         <div class="container">
@@ -46,18 +46,19 @@
                         class="content-card"
                         :key="edu.role"
                         v-if="!edu.hidecv">
-                        <div class="timeperiod">
-                            {{ formatDate(edu.startdate) }} -
-                            {{ edu.enddate !== null ? formatDate(edu.enddate) : $t("timeline.present", 1, {locale: "en"}) }}
-                        </div>
-                        <div class="text-base font-bold">
-                            <span v-if="edu.type === 'Minor'">{{ $t(edu.type, 1, {locale: "en"}) }}</span>
+                        <div class="text-base font-bold flex justify-between">
                             {{ $t(edu.role, 1, {locale: "en"}) }}
+                            <div class="timeperiod">
+                                {{ formatDate(edu.startdate) }} -
+                                {{
+                                    edu.enddate !== null ? formatDate(edu.enddate) : $t("timeline.present", 1, {locale: "en"})
+                                }}
+                            </div>
                         </div>
                         <div class="text-xs font-bold">{{ edu.company }}</div>
                         <div class="leading-[1.4]">{{ $t(edu.desc, 1, {locale: "en"}) }}</div>
                         <div class="flex flex-row flex-wrap gap-1">
-                            <div v-for="stack in edu.stack" class="border rounded-lg px-2 py-0.5 w-fit">{{ getSkillDisplayName(stack) }}</div>
+                            <Chip v-for="stack in edu.stack" :text="getSkillDisplayName(stack)"/>
                         </div>
                     </div>
                 </template>
@@ -70,41 +71,29 @@
                     class="content-card"
                     v-for="xp in getExperience()"
                     :key="xp.role">
-                    <div class="timeperiod">
-                        {{ formatDate(xp.startdate) }} -
-                        {{ xp.enddate !== null ? formatDate(xp.enddate) : $t("timeline.present", 1, {locale: "en"}) }}
+                    <div class="text-base font-bold flex justify-between">
+                        {{ $t(xp.role, 1, {locale: "en"}) }}
+                        <div class="timeperiod">
+                            {{ formatDate(xp.startdate) }} -
+                            {{
+                                xp.enddate !== null ? formatDate(xp.enddate) : $t("timeline.present", 1, {locale: "en"})
+                            }}
+                        </div>
                     </div>
-                    <div class="text-base font-bold">{{ $t(xp.role, 1, {locale: "en"}) }}</div>
                     <div class="text-xs font-bold">{{ xp.company }}</div>
                     <div class="leading-[1.4]">{{ $t(xp.desc, 1, {locale: "en"}) }}</div>
                     <div class="flex flex-row flex-wrap gap-1">
-                        <div v-for="stack in xp.stack" class="border rounded-lg px-2 py-0.5 w-fit">{{ getSkillDisplayName(stack) }}</div>
+                        <Chip v-for="stack in xp.stack" :text="getSkillDisplayName(stack)"/>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="flex flex-row flex-wrap md:flex-nowrap print:flex-nowrap">
-            <div class="w-full flex flex-col gap-2">
-                <div class="flex gap-1 items-center bg-gray-200 rounded-[20px_5px] p-2 uppercase font-bold">
-                    <span class="inline-block w-[200px]">Languages</span>
-                    <IconWrapper v-for="icon in languageIcons" :name="icon" size="24"/>
+        <div class="w-full flex flex-col gap-2">
+            <div v-for="skills in skillsSet" class="flex gap-1 items-center bg-gray-200 rounded-[20px_5px] p-1">
+                <span class="inline-block w-[225px] uppercase font-bold p-1">{{ skills.name }}</span>
+                <div class="flex flex-wrap gap-1 bg-white p-1 pl-2 w-full rounded-[15px_4px]">
+                    <Chip v-for="stack in skills.skills" :text="getSkillDisplayName(stack)"/>
                 </div>
-                <div class="flex gap-1 items-center bg-gray-200 rounded-[20px_5px] p-2 uppercase font-bold">
-                    <span class="inline-block w-[200px]">Frontend Frameworks</span>
-                    <IconWrapper v-for="icon in frontendFrameworkIcons" :name="icon" size="24"/>
-                </div>
-                <div class="flex gap-1 items-center bg-gray-200 rounded-[20px_5px] p-2 uppercase font-bold">
-                    <span class="inline-block w-[200px]">Backend Frameworks</span>
-                    <IconWrapper v-for="icon in backendFrameworkIcons" :name="icon" size="24"/>
-                </div>
-                <div class="flex gap-1 items-center bg-gray-200 rounded-[20px_5px] p-2 uppercase font-bold">
-                    <span class="inline-block w-[200px]">Databases & Tools</span>
-                    <IconWrapper v-for="icon in databasesIcons" :name="icon" size="24"/>
-                </div>
-            </div>
-            <div class="text-center text-sm my-0 basis-1/3">
-                <nuxt-img src="/img/company/qr.png" alt="qrcode" width="160" height="160" class="mx-auto"/>
-                <div>https://www.omererdem.nl</div>
             </div>
         </div>
     </div>
@@ -113,18 +102,39 @@
 <script setup>
 import timeline from "../assets/json/timeline.json";
 import skills from "../assets/json/skills.json";
-import IconWrapper from "../components/elements/IconWrapper";
 import moment from "moment/moment";
 import {DATE_FORMAT, RAW_DATE_FORMAT} from "../composables/useGitFlow";
+import Chip from "../components/elements/Chip";
 
-const languageIcons = ["js", "php", "java", "python", "csharp", "ts", "html", "css"]
-const frontendFrameworkIcons = ["vue", "nuxt", "angular", "react", "jquery", "alpine", "knockout", "sass", "tailwind", "bootstrap"]
-const backendFrameworkIcons = ["magento", "node", "spring", "laravel", "kotlin", "quarkus", "fastapi", "aspnet"]
-const databasesIcons = ["mysql", "mongodb", "firebase", "eventstore", "git", "docker"]
+const skillsSet = [
+    {
+        name: "Languages",
+        skills: ["js", "php", "java", "python", "csharp", "ts", "html", "css"]
+    },
+    {
+        name: "Frontend Frameworks",
+        skills: ["vue", "nuxt", "angular", "react", "jquery", "alpine", "sass", "tailwind"]
+    },
+    {
+        name: "Backend Frameworks",
+        skills: ["magento", "node", "spring", "laravel", "kotlin", "quarkus", "fastapi", "aspnet"]
+    },
+    {
+        name: "Databases & Tools",
+        skills: ["mysql", "mongodb", "firebase", "eventstore", "git", "docker", "Figma"]
+    },
+    {
+        name: "Experience",
+        skills: ["CRUD", "MVC", "OOP", "API", "SPA", "Design Patterns", "Algorithms", "Machine Learning", "Automation", "Google Tagmanager", "Core Web Vitals", "E-commerce"]
+    },
+    {
+        name: "Hobby Projects",
+        skills: ["Portfolio pages", "Landing pages", "Dashboards", "Automation Scripts", "Experiments"]
+    }
+]
 
 const getEducation = () => timeline.filter((x) => x.type === "Study")
 const getExperience = () => timeline.filter((x) => x.type === "Internship" || x.type === "Work").slice(0, 4)
-const getCVSkills = () => skills.filter((x) => x.show_cv === true)
 const formatDate = (date) => moment(date, RAW_DATE_FORMAT).format(DATE_FORMAT);
 const getSkillDisplayName = (skill) => skills.find(x => x.name === skill) ? skills.find(x => x.name === skill).display_name : skill;
 </script>
@@ -134,29 +144,37 @@ const getSkillDisplayName = (skill) => skills.find(x => x.name === skill) ? skil
     size: A4;
     margin: 0;
 }
+
 html, body, #app, #cv {
     @apply md:max-w-[210mm] md:max-h-[297mm] flex flex-col text-xs text-primary-text-dark p-4 bg-white;
     font-family: Arial, serif;
 }
+
 #cv .container {
     @apply mb-3 pt-1 pb-2.5 px-2.5 rounded-[50px_15px] w-full sm:max-w-full bg-gray-200;
 }
+
 #cv .container .container-title {
     @apply text-xl font-bold text-center pb-1;
 }
+
 #cv .container .content {
     @apply grid justify-between p-2.5 rounded-[30px_15px_40px] gap-4 bg-white;
 }
+
 .container .content {
     @apply sm:grid-cols-2;
 }
+
 .container.skills-container .content {
     @apply md:grid-cols-3 print:grid-cols-3;
 }
+
 #cv .container .content .content-card {
     @apply flex flex-col gap-0.5;
 }
+
 #cv .container .content .content-card .timeperiod {
-    @apply inline-flex text-xs font-bold px-2 py-1 rounded-[15px_5px] bg-gray-200 w-fit;
+    @apply inline-flex text-xs font-bold px-2 py-1 rounded-[15px_5px] bg-gray-200 w-fit whitespace-nowrap h-fit;
 }
 </style>
